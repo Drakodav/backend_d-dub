6280732421c48b1c935a024d95bcd09376a3a63b
drakodav <vmedves@gmail.com>
2020-12-10 12:15:28 +0000

    feat📓: add cors && allow all

--- dynamoDub/settings.py
+++ dynamoDub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'corsheaders',
     'rest_framework',
     'rest_framework_gis',
     'django_filters',
@@ -55,9 +56,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # serve static files
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["https://example.com"]
 
 ROOT_URLCONF = 'dynamoDub.urls'
 
