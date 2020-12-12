1ecd6c90dbc0b5cf9429d1238d62716dc07681c2
drakodav <vmedves@gmail.com>
2020-12-12 12:45:54 +0000

    fix: npm instead of yarn for deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,12 @@
 
 # shh in our server
 echo "########### connecting to server and run commands in sequence ###########"
-sudo ssh -i web-mapping_key.pem azureuser@40.121.42.196 \
+sudo ssh -i ../../web-mapping_key.pem azureuser@40.121.42.196 \
 '
 git clone https://github.com/Drakodav/backend_d-dub.git;
 sudo find backend_d-dub/ -type f -iname "*.sh" -exec chmod +x {} \;
 cd backend_d-dub/scripts;
-sudo ./django.sh;
+# sudo ./django.sh;
 sudo ./nginx.sh;
 cd ../.. ;
 sudo rm -r backend_d-dub;
