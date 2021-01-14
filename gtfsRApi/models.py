8e2e403e2e3153434a5f1a0587b4a558c934f2f0
drakodav <vmedves@gmail.com>
2021-01-14 15:57:55 +0000

    commit: add index

--- gtfsRApi/models.py
+++ gtfsRApi/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.indexes import HashIndex
 from django.utils import timezone
 
 # Create your models here.
@@ -7,7 +7,10 @@ from django.utils import timezone
 
 class GtfsRApi(models.Model):
     data = models.JSONField()
-    timestamp = models.DateTimeField(default=timezone.now)
+    timestamp = models.DateTimeField(default=timezone.now, db_index=True)
 
     def __str__(self):
         return 'gtfsRApi {}'.format(self.timestamp)
+
+    class Meta:
+        indexes = [HashIndex(fields=['timestamp'])]
