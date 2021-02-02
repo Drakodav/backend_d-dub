3a6d13742fb21f107eee694da5c82385091e2e20
drakodav <vmedves@gmail.com>
2021-02-02 13:16:55 +0000

    feat: database transactions

--- gtfsApi/tasks.py
+++ gtfsApi/tasks.py
@@ -1,89 +1,41 @@
 from celery.app import shared_task
 from django.db import connection
+from .query import delete_agency_query, delete_model_query
+from django.db import transaction
+from multigtfs.models import Agency
 
 
 @shared_task(ignore_result=False, track_started=True)
 def delete_model():
+    cursor = connection.cursor()
     try:
-        cursor = connection.cursor()
-        cursor.execute(
-            """
-            TRUNCATE 
-                Agency, Block, Fare_Rules, Fare, Feed_Info, Feed, Frequency, Route,
-                Service_Date, Service, Shape_Point, Shape, Stop_Time, Stop, Transfer, Trip, Zone;
-            """
-        )
-        cursor.close
-        return 'success'
+        with transaction.atomic():
+            with transaction.atomic():
+                cursor.execute(delete_model_query())
+
     except Exception as e:
-        return e
+        cursor.close
+        raise e
+
+    cursor.close
+    return 'success'
 
 
 @shared_task(ignore_result=False, track_started=True)
-def delete_agency(id):
+def delete_agency(id: int):
 
-    queries = [
-        # delete stop_time first            !!!revise
-        """
-        delete from stop_time
-        where exists (
-            select stimes.* 
-            from stop_time as stimes, (
-                select trips.id
-                from trip as trips, (
-                    select agency_id, id
-                    from  route
-                    where agency_id = {}
-                    group by agency_id, id
-                ) as my_routes
-                where my_routes.id = trips.route_id
-            ) as my_trips
-            where stimes.trip_id = my_trips.id
-        );
-        """.format(id),
-        # delete trip           !!! revise
-        """
-        delete from trip
-        where exists (
-            select trips.* 
-            from trip as trips, (
-                select agency_id, id
-                from  route
-                where agency_id = 168
-                group by agency_id, id
-            ) as my_routes
-            where my_routes.id = trips.route_id
-        );
-        """.format(id),
-        # delete routes not finished yet
-        """delete from route where agency_id = {};""".format(id),
-        # delete agency
-        """delete from agency where id = {};""".format(id),
+    cursor = connection.cursor()
+    try:
+        with transaction.atomic():
+            if Agency.objects.filter(id=id).exists():
+                with transaction.atomic():
+                    cursor.execute(delete_agency_query(id))
+            else:
+                cursor.close
+                return 'id: {}, does not exist in the database'.format(id)
+    except Exception as e:
+        cursor.close
+        raise e
 
-        # delete shape_points       !revise
-        """
-        DELETE FROM shape_point
-        WHERE Exists (
-            select points.*
-            from shape_point as points, (
-                select shapes.id
-                from shape as shapes, (
-                    select trips.route_id, trips.shape_id
-                    from trip as trips, (
-                        select agency_id, id
-                        from  route
-                        where agency_id = {}
-                        group by agency_id, id
-                    ) as my_routes
-                    where my_routes.id = trips.route_id
-                    group by route_id, shape_id
-                ) as my_trips
-                where shapes.id = my_trips.shape_id
-                group by shapes.id
-            ) as my_shapes
-            where points.shape_id = my_shapes.id
-        );
-        """.format(id),
-    ]
-
+    cursor.close
     return 'success'
