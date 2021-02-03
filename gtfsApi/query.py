6ed2d0fc51bbf54431ec802bcd0231556fc5529c
drakodav <vmedves@gmail.com>
2021-02-03 13:12:47 +0000

    fix: proper query

--- gtfsApi/query.py
+++ gtfsApi/query.py
@@ -1,60 +1,47 @@
 
 def route_stops_query(id: int, direction: int = 0):
     return """
-SELECT stop.*, unique_stops.stop_sequence
-FROM stop, (
-	SELECT stop_id, route_id, stop_sequence
-	FROM stop_time,(
-		select 
-			my_trips.*,
-			CAST(
-				CASE 
-					WHEN extract(dow from current_date)=0 THEN services.sunday
-					WHEN extract(dow from current_date)=1 THEN services.monday
-					WHEN extract(dow from current_date)=2 THEN services.tuesday 
-					WHEN extract(dow from current_date)=3 THEN services.wednesday 
-					WHEN extract(dow from current_date)=4 THEN services.thursday
-					WHEN extract(dow from current_date)=5 THEN services.friday 
-					WHEN extract(dow from current_date)=6 THEN services.saturday 
-				END as bool
-			) as dow
-		from service as services, (
-			select id, route_id, direction, service_id
-			from trip as trips
-			where trips.route_id = {} and direction = '{}'
-			GROUP BY shape_id, id, route_id, direction, service_id
-		) as my_trips
-		where my_trips.service_id = services.id
-	) AS unique_trips
-	WHERE 
-		stop_time.trip_id = unique_trips.id 
-		and 
-		to_timestamp(arrival_time)::time >= current_time(0)::time
-		and 
-		unique_trips.dow = true
-	GROUP BY stop_id, route_id, stop_time.stop_sequence, stop_time.arrival_time
-	order by stop_time.stop_sequence, to_timestamp(arrival_time)::time
-	) AS unique_stops
-WHERE stop.id = unique_stops.stop_id
-group by stop.stop_id, stop.id, unique_stops.stop_sequence
-order by unique_stops.stop_sequence
+select 
+ 	 stop.id, stop.stop_id, stop.name, stop.point, stop_time.stop_sequence
+from stop
+left join stop_time on stop_time.stop_id = stop.id
+left join trip on trip.id = stop_time.trip_id
+left join service on service.id = trip.service_id
+, (
+	select 
+		id,
+		CAST(
+			CASE
+				WHEN extract(dow from current_date)=0 THEN services.sunday
+				WHEN extract(dow from current_date)=1 THEN services.monday
+				WHEN extract(dow from current_date)=2 THEN services.tuesday
+				WHEN extract(dow from current_date)=3 THEN services.wednesday
+				WHEN extract(dow from current_date)=4 THEN services.thursday
+				WHEN extract(dow from current_date)=5 THEN services.friday
+				WHEN extract(dow from current_date)=6 THEN services.saturday
+			END as bool
+		) as dow
+	from service as services
+	group by id, dow
+) as my_service, (
+	select my_trips.id
+	from stop_time, (
+		select trip.id
+		from trip
+		where trip.route_id = {} and trip.direction = '{}'
+		group by trip.id
+	) as my_trips
+	where to_timestamp(stop_time.departure_time)::time >= current_time(0)::time
+		and stop_time.stop_sequence = 1
+		and stop_time.trip_id = my_trips.id
+	order by stop_time.departure_time
+	limit 10
+) as one_trip
+where my_service.id = service.id 
+	and my_service.dow = true
+	and trip.service_id = my_service.id 
+ 	and trip.id = one_trip.id
+group by stop.id, stop_time.stop_sequence
+order by stop_time.stop_sequence
 ;
 """.format(id, direction).lstrip()
-
-
-# """
-# SELECT stop.*
-# FROM stop, (
-# 	SELECT stop_id, route_id
-# 	FROM stop_time,(
-# 		SELECT id, route_id, direction
-# 		FROM trip
-# 		WHERE route_id = {} and direction = '{}'
-# 		GROUP BY shape_id, id, route_id, direction
-# 	) AS unique_trips
-# 	WHERE stop_time.trip_id = unique_trips.id
-# 	GROUP BY stop_id, route_id
-# 	) AS unique_stops
-# WHERE stop.id = unique_stops.stop_id
-# group by stop.stop_id, stop.id;
-# """.format(id, direction).lstrip()
