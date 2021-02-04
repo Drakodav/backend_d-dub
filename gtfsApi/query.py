2fb71f431c1ad1c40ed55c7986c7f0511865d47c
drakodav <vmedves@gmail.com>
2021-02-04 18:18:13 +0000

    feat: departure query

--- gtfsApi/query.py
+++ gtfsApi/query.py
@@ -7,6 +7,7 @@ from stop
 left join stop_time on stop_time.stop_id = stop.id
 left join trip on trip.id = stop_time.trip_id
 left join service on service.id = trip.service_id
+left join service_date on service.id = service_date.service_id
 , (
 	select 
 		id,
@@ -41,7 +42,51 @@ where my_service.id = service.id
 	and my_service.dow = true
 	and trip.service_id = my_service.id 
  	and trip.id = one_trip.id
+	and not service.id in (select service_id from service_date where date = current_date )
 group by stop.id, stop_time.stop_sequence
 order by stop_time.stop_sequence
 ;
 """.format(id, direction).lstrip()
+
+
+def stop_departures_query(stop_id: int):
+    return """
+select 
+	to_timestamp(stop_time.departure_time)::time,
+	route.short_name,
+ 	trip.id, 
+ 	trip.trip_id, 
+ 	trip.headsign, 
+ 	trip.direction, 
+ 	trip.geometry
+from trip
+left join stop_time on trip.id = stop_time.trip_id
+left join service on trip.service_id = service.id
+left join service_date on service.id = service_date.service_id
+left join route on trip.route_id = route.id
+, (
+	select 
+		id,
+		CAST(
+			CASE
+				WHEN extract(dow from current_date)=0 THEN services.sunday
+				WHEN extract(dow from current_date)=1 THEN services.monday
+				WHEN extract(dow from current_date)=2 THEN services.tuesday
+				WHEN extract(dow from current_date)=3 THEN services.wednesday
+				WHEN extract(dow from current_date)=4 THEN services.thursday
+				WHEN extract(dow from current_date)=5 THEN services.friday
+				WHEN extract(dow from current_date)=6 THEN services.saturday
+			END as bool
+		) as dow
+	from service as services
+	group by id, dow
+) as my_service
+where stop_time.stop_id = {}
+	and to_timestamp(stop_time.departure_time)::time >= current_time(0)::time
+	and my_service.dow = true
+	and trip.service_id = my_service.id 
+	and not service.id in (select service_id from service_date where date = current_date )
+group by trip.id, stop_time.id, route.id
+order by stop_time.departure_time
+;
+""".format(stop_id).lstrip()
