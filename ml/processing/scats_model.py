02481d66c98b541d8cc82c1318192dee6fd9a5ec
drakodav <vmedves@gmail.com>
2021-02-15 20:21:34 +0000

    randomForest

---
+++ ml/processing/scats_model.py
@@ -0,0 +1,82 @@
+from sklearn.ensemble import RandomForestRegressor
+from vaex.ml.sklearn import Predictor
+import vaex.ml
+import vaex
+import time
+import os
+
+dir = os.path.dirname(__file__)
+outdir = os.path.join(dir, 'output')
+csv = os.path.join(dir, 'output', 'processed_scats.csv')
+model_out = os.path.join(outdir, 'scats_model.json')
+
+
+# we create the scats model which can be used to predict unkown avg_volumes of traffic for a
+# lat: radians, lon: radians, dayOfWeek: int, hourOfDay: int
+def create_scats_ml_model():
+
+    start = time.time()
+    print('starting scats ml modeling')
+
+    # load existing csv into vaex dataframe
+    if not os.path.exists(csv+'.hdf5'):
+        df = vaex.from_csv(csv, convert=True,
+                           copy_index=False, chunk_size=1000000)
+        df.export(csv, shuffle=True)
+    df = vaex.open(csv+'.hdf5', shuffle=True)
+    df = df.sample(frac=1)
+
+    # transform the features into more machine learning friendly vars
+    pca_coord = vaex.ml.PCA(
+        features=['lat', 'lon'], n_components=2, prefix='pca')
+    df = pca_coord.fit_transform(df)
+
+    cycl_transform_hour = vaex.ml.CycleTransformer(features=['hour'], n=24)
+    df = cycl_transform_hour.fit_transform(df)
+
+    cycl_transform_dow = vaex.ml.CycleTransformer(features=['dow'], n=7)
+    df = cycl_transform_dow.fit_transform(df)
+
+    cycl_transform_day = vaex.ml.CycleTransformer(features=['day'], n=31)
+    df = cycl_transform_day.fit_transform(df)
+
+    # split into train and test
+    df_train, df_test = df.ml.train_test_split(test_size=0.2, verbose=False)
+
+    feats = df_train.get_column_names(regex='pca') + \
+        df_train.get_column_names(regex='.*_x') + \
+        df_train.get_column_names(regex='.*_y')
+    target = 'avg_vol'
+
+    print('dataWrangling done, ready to create model, time: {}s'.format(
+        round(time.time() - start)))
+
+    # create a randomForestRegression model
+    model = RandomForestRegressor(
+        random_state=42, n_estimators=7*24, oob_score=True)
+    vaex_model = Predictor(
+        features=feats,
+        target=target,
+        model=model,
+        prediction_name='p_avg_vol'
+    )
+
+    # here we fit and train the model
+    vaex_model.fit(df=df_train)
+
+    print('model created, time: {}s'.format(round(time.time() - start)))
+
+    # trained model
+    df_train = vaex_model.transform(df_train)
+
+    df_train.state_write(model_out)
+    # df_train.state_load('./output/scats_model.json')
+
+    print('model written to output, finished, time: {}s'.format(
+        round(time.time() - start)))
+    return
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    create_scats_ml_model()
