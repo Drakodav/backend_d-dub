014a348782cf5646e9b1dabcdaea899ad9772d9b
drakodav <vmedves@gmail.com>
2021-02-24 21:25:40 +0000

    update: gtfsr more frequent, better live updates

--- dynamoDub/celery.py
+++ dynamoDub/celery.py
@@ -3,15 +3,15 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dynamoDub.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dynamoDub.settings")
 
-app = Celery('dynamoDub')
+app = Celery("dynamoDub")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
@@ -20,13 +20,13 @@ app.conf.beat_schedule = {
     # Executes every 80 seconds.
     "gtfs_r_api": {
         "task": "gtfsRApi.tasks.gtfs_r_api",
-        "schedule": 62.0,  # run every X seconds
+        "schedule": 31.0,  # run every X seconds
     }
 }
 
 app.conf.broker_transport_options = {
-    'max_retries': 3,
-    'interval_start': 0,
-    'interval_step': 0.2,
-    'interval_max': 0.5,
+    "max_retries": 3,
+    "interval_start": 0,
+    "interval_step": 0.2,
+    "interval_max": 0.5,
 }
