2fa273790f44ed978640d67f6c83470d9f7fb399
drakodav <vmedves@gmail.com>
2021-02-24 21:06:46 +0000

    return every 10 records: decrease load

--- gtfsRApi/tasks.py
+++ gtfsRApi/tasks.py
@@ -11,23 +11,22 @@ import json
 @shared_task
 def gtfs_r_api():
     headers = {
-        'Accept': 'application/json',
-        'Cache-Control': 'no-cache',
-        'x-api-key': os.getenv("X_API_KEY"),
+        "Accept": "application/json",
+        "Cache-Control": "no-cache",
+        "x-api-key": os.getenv("X_API_KEY"),
     }
 
     try:
-        response = requests.get(
-            url='https://gtfsr.transportforireland.ie/v1/?format=json', headers=headers)
+        response = requests.get(url="https://gtfsr.transportforireland.ie/v1/?format=json", headers=headers)
 
         data = response.json()
 
         if "statusCode" in data and data["statusCode"] == 429:
-            raise Exception('{}'.format(data["message"]))
+            raise Exception("{}".format(data["message"]))
         else:
             record = GtfsRApi(data=data)
             record.save()
-            return 'Success {}'.format(record.timestamp)
+            return "Success {}".format(record.timestamp)
     except Exception as e:
         return e
 
@@ -37,18 +36,18 @@ def download_realtime_data(self, year: int, month: int):
     try:
         if GtfsRApi.objects.filter(timestamp__year=year, timestamp__month=month).exists():
             progress_recorder = ProgressRecorder(self)
-            records = GtfsRApi.objects\
-                .filter(timestamp__year=year, timestamp__month=month)\
-                .values_list('data', flat=True)
+            records = GtfsRApi.objects.filter(timestamp__year=year, timestamp__month=month).values_list(
+                "data", flat=True
+            )
 
             length = records.count()
-            with zipfile.ZipFile(os.path.join(STATIC_ROOT, 'GtfsRRecords.zip'), 'w') as zf:
+            with zipfile.ZipFile(os.path.join(STATIC_ROOT, "GtfsRRecords.zip"), "w") as zf:
                 for i, record in enumerate(records.iterator(chunk_size=500)):
-                    zf.writestr("{}.json".format(i), json.dumps(record),
-                                compress_type=zipfile.ZIP_DEFLATED)
-                    progress_recorder.set_progress(i+1, length)
+                    if i % 10 == 0:
+                        zf.writestr("{}.json".format(i), json.dumps(record), compress_type=zipfile.ZIP_DEFLATED)
+                        progress_recorder.set_progress(i + 1, length)
 
-            return 'success'
+            return "success"
         else:
             return None
     except Exception as e:
