6ef3ac8b37446ef9b1b924ac307188d7793a3e74
drakodav <vmedves@gmail.com>
2021-03-02 23:29:05 +0000

    feat: working ml model #3

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,16 +3,37 @@
 # run from this directory
 cd "${BASH_SOURCE%/*}" || exit
 
-# copy .env file to VM
-sudo scp -i ../../web-mapping_key.pem \
-../.env \
-azureuser@40.121.42.196:/home/azureuser/
 
 # shh in our server
 echo "########### connecting to server and run commands in sequence ###########"
 sudo ssh -i ../../web-mapping_key.pem azureuser@40.121.42.196 \
 '
 git clone https://github.com/Drakodav/backend_d-dub.git;
+'
+
+# copy .env file to VM
+sudo scp -i ../../web-mapping_key.pem \
+../.env \
+azureuser@40.121.42.196:/home/azureuser/
+
+# copy machine learning files file to VM
+sudo scp -i ../../web-mapping_key.pem \
+../ml/processing/output/gtfsr_historical_means.hdf5 \
+azureuser@40.121.42.196:/home/azureuser/backend_d-dub/ml/processing/output/
+
+sudo scp -i ../../web-mapping_key.pem \
+../ml/processing/output/stop_time_data.hdf5 \
+azureuser@40.121.42.196:/home/azureuser/backend_d-dub/ml/processing/output/
+
+sudo scp -i ../../web-mapping_key.pem \
+../ml/processing/output/gtfsr_model.json \
+azureuser@40.121.42.196:/home/azureuser/backend_d-dub/ml/processing/output/
+
+
+# finish deploying django and cleaning up
+echo "deploying backend"
+sudo ssh -i ../../web-mapping_key.pem azureuser@40.121.42.196 \
+'
 sudo find backend_d-dub/ -type f -iname "*.sh" -exec chmod +x {} \;
 cp .env backend_d-dub/;
 cd backend_d-dub/scripts;
